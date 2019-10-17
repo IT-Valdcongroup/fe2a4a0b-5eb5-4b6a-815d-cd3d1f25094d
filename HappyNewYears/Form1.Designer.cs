82af46502a027101c7d2ed76db85db7ff168648e
Inginer S.P <valdcongroup@gmail.com>
2019-10-17 14:09:47 +0300

    Archived project

--- HappyNewYears/Form1.Designer.cs
+++ HappyNewYears/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HappyNewYears
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -29,12 +29,49 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 8000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::HappyNewYears.Properties.Resources.christmas_valdcongrup4;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.cLOSE);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoadMainForm);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- HappyNewYears/Form1.cs
+++ HappyNewYears/Form1.cs
@@ -1,20 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HappyNewYears
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public int counter = 1;
+        public List<Bitmap> collection;
+        public MainForm()
         {
             InitializeComponent();
+            collection = GetNextImage();
+            
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                      
+
+            if (counter > collection.Count-1)
+            counter = 0;
+            pictureBox1.Image = collection[counter];
+            counter++;
+        }
+
+     
+
+        public List<Bitmap> GetNextImage()
+        {
+           
+            Bitmap image1 = Properties.Resources.christmas_card_valdcon_grup4;
+            Bitmap image2 = Properties.Resources.christmas_card_valdcon_grup8;
+            Bitmap image3 = Properties.Resources.christmas_card_valdcon_grup12;
+            List<Bitmap> ImageList = new List<Bitmap>() { image1, image2, image3 };
+            return ImageList;
+        }
+
+        private void cLOSE(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LoadMainForm(object sender, EventArgs e)
+        {
+            System.IO.Stream str = Properties.Resources.new_york_new_york_v2__online_audio_converter_com_;
+            System.Media.SoundPlayer snd = new System.Media.SoundPlayer(str);
+            snd.PlayLooping();
+
         }
     }
 }
